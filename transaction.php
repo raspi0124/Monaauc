125a0cc6436b63b21b4741658abf2670d1d68a42
raspi0124 <adores0124@yahoo.co.jp>
2018-01-11 15:00:14 +0100

    Update transaction.php

--- transaction.php
+++ transaction.php
@@ -1,1 +1,9 @@
+<?php
+// Start transaction
+// Please define $username before you send transaction request.
+// it maybe better if it's like username_number
+$addresstosend = echo exec('monacoin-cli getaddressesbyaccount' $username'');
+
+
+
 
